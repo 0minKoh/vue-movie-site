dbe129194f6c11d754f2be2e971d93793f1659f3
0minKoh <esesang13@naver.com>
2022-05-30 08:34:05 +0900

    개발서버를 위한 임시저장

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,24 +1,38 @@
+import axios from "axios"
+
 export default {
   //module
   namespaced: true,
   //data
   state: () => ({
-    movies: []
+    movies: [],
+    message: '',
+    loading: false
   }),
   //computed
-  getters: {
-    movieIds(state) {
-      return state.movies.map(m => m.imdbID)
-    }
-  },
+  getters: {},
   //methods
   mutations: { //데이터 변경
+    updateState(state, payload) {
+      //새로운 배열 데이터 생성 .keys
+      Object.keys(payload).forEach(key => {
+        state[key] = payload[key]
+      })
+    },
     resetMovies(state) {
       state.movies = []
     }
   },
   actions: { //비동기 함수
-    searchMovies({}) {
+    searchMovies({commit}, payload) {
+      const {title, type, number, year} = payload
+      const OMDB_API_KEY = '7035c60c'
+
+      const res = axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+      const {Search, totalResults} = res.data
+      commit('updateState', {
+        movies: Search
+      })
     }
   }
 }
