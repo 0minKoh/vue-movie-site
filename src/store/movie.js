3e79ce78cb81bdde95a0c9c685f37b36981abf5f
0minKoh <esesang13@naver.com>
2022-05-30 10:34:06 +0900

    영화 검색 추가 요청

--- src/store/movie.js
+++ src/store/movie.js
@@ -24,7 +24,7 @@ export default {
     }
   },
   actions: { //비동기 함수
-    async searchMovies({commit}, payload) {
+    async searchMovies({state, commit}, payload) {
       const {title, type, number, year} = payload
       const OMDB_API_KEY = '7035c60c'
       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
@@ -33,8 +33,25 @@ export default {
       commit('updateState', {
         movies: Search
       })
-      console.log(totalResults)
-      console.log(typeof totalResults)
+      console.log(totalResults) //268
+      console.log(typeof totalResults) // string
+
+      const total = parseInt(totalResults, 10)
+      const pageLength = Math.ceil(total / 10) //ceil = 올림 //페이지 개수
+
+      //추가 요청
+      if (pageLength > 1) {
+        for (let page = 2; page <= pageLength; page += 1) {
+          if (page > (number / 10)) {
+            break
+          }
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+          const { Search } = res.data
+          commit('updateState', {
+            movies: [...state.movies, ...Search] //기존 데이터 유지, 새로운 데이터 추가
+          })
+        }
+      }
     }
   }
 }
